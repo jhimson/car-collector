a6c727e5b5104da8269c10866250d9e3e9640062
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-19 13:14:29 -0400

    Collector Lab - Part 2 - Complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=250)
+
+    # new code below
+    def __str__(self):
+        return self.make
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Car
 # ! CONTROLLER
 # Create your views here.
 
 # Add the Cat class & list and view function below the imports
 
 
-class Car:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, make, model, color):
-        self.make = make
-        self.model = model
-        self.color = color
+# class Car:  # Note that parens are optional if not inheriting from another class
+#     def __init__(self, make, model, color):
+#         self.make = make
+#         self.model = model
+#         self.color = color
 
 
-cars = [
-    Car('Toyota', 'Mustang Mach-E', 'White'),
-    Car('Audi', 'V8', 'Blue'),
-    Car('BMW', 'X5 Sports', 'White')
-]
+# cars = [
+#     Car('Toyota', 'Mustang Mach-E', 'White'),
+#     Car('Audi', 'V8', 'Blue'),
+#     Car('BMW', 'X5 Sports', 'White')
+# ]
 
 
 def home(request):
@@ -29,4 +30,10 @@ def about(request):
 
 
 def cars_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', {'cars': cars})
+
+
+def cars_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {'car': car})
